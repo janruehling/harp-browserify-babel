2186fab09c0516ab49828651caa6af5cd1308e14
Jan Ruehling <janruehling@gmail.com>
2016-06-21 11:09:31 +0200

    Change browsersync settings

--- bs-config.js
+++ bs-config.js
@@ -1,5 +1,7 @@
 module.exports = {
   reloadDelay: 1000,
   files: ['src/**/*.js', 'src/**/*.jade', 'src/**/*.scss'],
-  proxy: 'localhost:9000'
+  proxy: 'localhost:9000',
+  online: true,
+  notify: false
 }
